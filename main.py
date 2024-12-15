b5d56b391b6b65499679acf9db9158fc495e77e3
Avnish-Jassal <2024_avnish.jassal@iticbcn.cat>
2024-12-15 14:58:08 +0100

    Update main.py
    100%

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -6,10 +8,40 @@ app = FastAPI()
 def read_root():
     return {"message": "Hola, FastAPI!"}
 
+
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
+    if item_id <= 0:  # Simulació de que l'element no existeix
+        return {"status": 404, "detail": "Item s'ha no trobat"}
     return {"item_id": item_id, "name": f"Item {item_id}"}
+
+
+#### ES DE HTTPEXCEPTOIN
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int):
+#    if item_id <= 0:  # retorna un error 404
+#        raise HTTPException(status_code=404, detail="Item no trobat")
+#    return {"item_id": item_id, "name": f"Item {item_id}"}
+
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int):
+#    return {"item_id": item_id, "name": f"Item {item_id}"}
 
 @app.get("/users/{user_id}")
 def read_user(user_id: int):
     return {"user_id": user_id, "username": f"user{user_id}"}
+
+
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float
+    quantity: int
+    optional_field: str | None = None
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"message": "Item creat correctament", "item": item.dict()}
